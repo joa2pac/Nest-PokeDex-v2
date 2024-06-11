e0836e5e1a06548cbc81746269ec6cda72f22b76
joaquin <joaquinahuel93@gmail.com>
2024-06-11 08:25:03 -0300

    configuration service

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,8 +1,10 @@
 import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { isValidObjectId, Model } from 'mongoose';
-import { Pokemon } from './entities/pokemon.entity';
+import { ConfigService } from '@nestjs/config';
 
+import { isValidObjectId, Model } from 'mongoose';
+
+import { Pokemon } from './entities/pokemon.entity';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
@@ -10,13 +12,19 @@ import { PaginationDto } from 'src/common/dto/pagination.dto';
 @Injectable()
 export class PokemonService {
 
+  private defaultLimit: number;
+
   constructor(
     
     @InjectModel( Pokemon.name )
     private readonly pokemonModel: Model<Pokemon>,
 
+    private readonly configService: ConfigService
+
   ) {
-    console.log(process.env.DEFAULT_LIMIT);
+    
+    this.defaultLimit = configService.get<number>('defaultLimit')
+    
   }
 
 
@@ -39,7 +47,7 @@ export class PokemonService {
 
    
 
-    const { limit = 5, offset = 0 } = paginationDto;
+    const { limit = this.defaultLimit, offset = 0 } = paginationDto;
 
     return this.pokemonModel.find()
       .limit( limit )
